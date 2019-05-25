71bddeddaaebe808d5445c3c0af3b1f10e82abd3
Tim <Techne3@gmail.com>
2019-05-24 21:51:51 -0600

    added pictures

--- main.js
+++ main.js
@@ -28,43 +28,14 @@ var Now = moment().format('ll');
 var x = document.getElementById('date');
 x.innerHTML = Now;
 
-// tmonth = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+// delete text under clock
 
-// function GetClock() {
-//     var d = new Date(+new Date + 12096e5);
-//     var dx = d.toGMTString();
-//     dx = dx.substr(0, dx.length - 3);
-//     d.setTime(Date.parse(dx))
-//     d.setSeconds(d.getSeconds() + 0);
-//     var nday = d.getDay(),
-//         nmonth = d.getMonth(),
-//         ndate = d.getDate(),
-//         nyear = d.getYear(),
-//         nhour = d.getHours(),
-//         nmin = d.getMinutes(),
+var textDelete = document.querySelector('.delete-text');
 
-//         ap;
+textDelete.addEventListener('click', function(){
+	textDelete.style.display = 'none';
+});
 
-//     if (nhour == 0) {
-//         ap = " AM";
-//         nhour = 12;
-//     } else if (nhour < 12) {
-//         ap = " AM";
-//     } else if (nhour == 12) {
-//         ap = " PM";
-//     } else if (nhour > 12) {
-//         ap = " PM";
-//         nhour -= 12;
-//     }
+// github pull request 
 
-//     if (nyear < 1000) nyear += 1900;
-//     if (nmin <= 9) nmin = "0" + nmin;
-
-//     document.getElementById('time').innerHTML = "" + nhour + ":" + nmin ;
-//     document.getElementById('hour').innerHTML = "" + ap + "";
-//     document.getElementById('date').innerHTML = "" + tmonth[nmonth].toUpperCase() + " " + ndate + ", " + nyear + "";
-// }
-
-// window.onload = function () {
-//     GetClock();
-//     setInterval(GetClock, 1000);
+var 
